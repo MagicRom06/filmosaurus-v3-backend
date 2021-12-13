0483f23b7201e04237ffdeb0728260be99a1e2dd
MagicRom06 <romainhenry60@gmail.com>
2021-12-13 21:53:48 +0100

    add db postgres

--- core/settings.py
+++ core/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('PG_NAME'),
+        'USER': env('PG_USER'),
+        'PASSWORD': env('PG_PASSWORD'),
+        'HOST': env('PG_HOST'),
+        'PORT': env('PG_PORT')
     }
 }
 
