71f04e66710b4a0d3d0c760615b81f16fe5eeb3a
MagicRom06 <romainhenry60@gmail.com>
2021-12-13 23:02:28 +0100

    add test for users app

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+
+class CustomUserTest(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create(
+            email='test@test.com',
+            password='test12345'
+        )
+        self.assertEqual(user.email, 'test@test.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+    
+    def test_create_superuser(self):
+        User = get_user_model()
+        user = User.objects.create_superuser(
+            username='test',
+            email='supertest@test.com',
+            password='test12345'
+        )
+        self.assertEqual(user.email, 'supertest@test.com')
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
