e699a4a606974e633cfe21b0225cadc8efac6c7b
Romain HENRY <romainhenry@macbook-pro-de-romain.home>
2021-12-17 13:56:03 +0100

    fix test for users app

--- users/tests.py
+++ users/tests.py
@@ -7,9 +7,11 @@ class UsersManagersTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
         user = User.objects.create_user(
+            username='test',
             email='normal@user.com',
             password='foo'
         )
+        self.assertEqual(user.username, 'test')
         self.assertEqual(user.email, 'normal@user.com')
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
@@ -18,9 +20,11 @@ class UsersManagersTests(TestCase):
     def test_create_superuser(self):
         User = get_user_model()
         user = User.objects.create_superuser(
+            username='supertest',
             email='super@user.com',
             password='foo'
         )
+        self.assertEqual(user.username, 'supertest')
         self.assertEqual(user.email, 'super@user.com')
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
