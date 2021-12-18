a91b01bfc525a08c4fad37328dab1ae741fbf9ea
Romain HENRY <romainhenry@MacBook-Pro-de-Romain.local>
2021-12-18 16:12:53 +0100

    add gunicorn

--- core/settings.py
+++ core/settings.py
@@ -26,6 +26,7 @@ if env('ENV') == 'production':
     # SECURE_CONTENT_TYPE_NOSNIFF = True
     # SESSION_COOKIE_SECURE = True
     # CSRF_COOKIE_SECURE = True
+    # SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 else:
     DEBUG=True
 
