a357a3828c4b01ef230f741878c59d8fd93b0414
Romain HENRY <romainhenry@MacBook-Pro-de-Romain.local>
2021-12-18 14:54:32 +0100

    add docker-compose for prod

--- core/settings.py
+++ core/settings.py
@@ -12,14 +12,16 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
 import environ
+
+ENVIRONMENT = os.environ.get('ENVIRONMENT', default='production')
 
 env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
